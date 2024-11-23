db1cfca9aa97f5e009cc3b943300ebd68ae45408
amitaizenman <amit.aizenman1@mail.huji.ac.il>
2024-11-23 20:35:59 +0200

    added PlayerMovement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private float _horizontalMovement;
+    private float _verticalMovement;
+    [SerializeField] private float force;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+     _rigidbody = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _horizontalMovement = Input.GetAxis("Horizontal");
+        _verticalMovement = Input.GetAxis("Vertical");
+        
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.AddForce(Vector3.right * _horizontalMovement * Time.fixedDeltaTime * force);
+        _rigidbody.AddForce(Vector3.up * _verticalMovement * Time.fixedDeltaTime * force);
+    }
+}
