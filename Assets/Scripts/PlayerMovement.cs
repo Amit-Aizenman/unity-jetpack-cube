1e14ea7850a7f99636ff89152538ee42975d7eff
amitaizenman <amit.aizenman1@mail.huji.ac.il>
2024-11-23 21:47:09 +0200

    changed key to spacebar and added more variables to PlayerMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,17 @@
 using System;
+using System.Drawing;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody _rigidbody;
     private float _horizontalMovement;
-    private float _verticalMovement;
+    private bool _verticalMovement;
     [SerializeField] private float force = 1000;
+    private int _score = 0;
+    private int _initialHealth = 100;
+    private int _maxSpeed = 100;
+    private int _maxFlySpeed = 100;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,13 +23,30 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         _horizontalMovement = Input.GetAxis("Horizontal");
-        _verticalMovement = Input.GetAxis("Vertical");
+        _verticalMovement = Input.GetKey(KeyCode.Space);
         
     }
 
     private void FixedUpdate()
     {
-        _rigidbody.AddForce(Vector3.right * _horizontalMovement * Time.fixedDeltaTime * force);
-        _rigidbody.AddForce(Vector3.up * _verticalMovement * Time.fixedDeltaTime * force);
+        if (_rigidbody.linearVelocity.x < _maxSpeed)
+        {
+            _rigidbody.AddForce(Vector3.right * (_horizontalMovement * Time.fixedDeltaTime * force));
+        }
+
+        if (_rigidbody.linearVelocity.y < _maxFlySpeed && _verticalMovement)
+        {
+            _rigidbody.AddForce(Vector3.up * (Time.fixedDeltaTime * force));
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Point"))
+        {
+            var point = other.gameObject.GetComponent<Point>();
+            Destroy(other.gameObject);
+            _score += 1;
+        }
     }
 }
