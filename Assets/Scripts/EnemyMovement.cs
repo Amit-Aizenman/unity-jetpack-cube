ccf353f2c95aae7211482616ed91cc4ec9767c25
amitaizenman <amit.aizenman1@mail.huji.ac.il>
2024-11-25 10:57:13 +0200

    made the enemies move

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    private bool _movementFlag = false;
-    [SerializeField] private bool _horizontalMovementFlag = true; // True if vertical, false if horizontal
+    private int _movementFlag = 1;
+    [SerializeField] bool _horizontalMovementFlag = true; // True if vertical, false if horizontal
+    [SerializeField] private float speed = 3;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,18 +17,25 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (_horizontalMovementFlag){
+            this.transform.position += Vector3.right * (Time.deltaTime * _movementFlag *speed);
+        }
+        else
+        {
+            this.transform.position += Vector3.up * (Time.deltaTime * _movementFlag * speed);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (_movementFlag)
+        Console.WriteLine("hi");
+        if (_movementFlag == 1)
         {
-            _movementFlag = false;
+            _movementFlag = 0;
         }
         else
         {
-            _movementFlag = true;
+            _movementFlag = 1;
         }
     }
 }
