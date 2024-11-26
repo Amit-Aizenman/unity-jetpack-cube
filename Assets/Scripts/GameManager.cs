8672d09f56c815db5d2b0bd2f1e8794ddea7933e
amitaizenman <amit.aizenman1@mail.huji.ac.il>
2024-11-26 12:57:20 +0200

    added quit function in gamemanager and win condition

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private int _playerScore = 0;
+    [SerializeField] int _pointToWin = 3;
     private void OnEnable()
     {
         MyEvents.OnPointCollected += HandlePointCollected;
@@ -17,10 +18,29 @@ public class GameManager : MonoBehaviour
     private void HandlePointCollected(int points)
     {
         _playerScore += points;
+        print("Point Collected! Current score is " + _playerScore);
+        if (points >= _pointToWin)
+        {
+            print ("Congratulations! You won!");
+            QuitGame();
+        }
     }
 
     public int GetPlayerScore()
     {
         return _playerScore;
     }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+        // Application.Quit() does not work in the editor
+        // so we use this instead
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        // Close the game!
+        Application.Quit();
+        #endif
+    }
+
 }
